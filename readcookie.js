5e89b6051146c05ace4e9c311766d81311976bc1
githubcyclist <mouse869@gmail.com>
2016-07-20 17:30:22 -0400

    Added code to read cookies for pull-ups

--- readcookie.js
+++ readcookie.js
@@ -21,9 +21,13 @@ var thisisdacookie2 = "  Time spent doing push-ups: " + getCookie("timespentpush
 var thisisdacookie3 = "  Push-ups done " + getCookie("pushupsdone") + " times";
 var thisisdacookie4 = "  Jumped rope for " + getCookie("timespentjumprope") + " minutes";
 var thisisdacookie5 = "  Jumped rope " + getCookie("timesjumpedrope") + " times";
+var thisisdacookie6 = "  Time spent doing pull-ups: " + getCookie("timespentpullups") + " minutes";
+var thisisdacookie7 = "  Pull-ups done " + getCookie("pullupsdone") + " times";
 document.getElementById('timespentpushups').innerHTML=thisisdacookie2;
 document.getElementById('pushupsdone').innerHTML=thisisdacookie3;
 document.getElementById('timeoutside').innerHTML=thisisdacookie;
 document.getElementById('minjumpedrope').innerHTML=thisisdacookie4;
 document.getElementById('timesjumpedrope').innerHTML=thisisdacookie5;
+document.getElementById('minjumpedrope').innerHTML=thisisdacookie6;
+document.getElementById('timesjumpedrope').innerHTML=thisisdacookie7;
 }
