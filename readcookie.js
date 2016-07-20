78aa4f12ee8268b468fc5f83e51c54ac40a10fb0
githubcyclist <mouse869@gmail.com>
2016-07-20 08:13:31 -0400

    Update readcookie.js

--- readcookie.js
+++ readcookie.js
@@ -1,1 +1,20 @@
-
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+	var c = ca[i];
+	while (c.charAt(0)==' ') {
+		c = c.substring(1);
+	}
+	if (c.indexOf(name) == 0) {
+		return c.substring(name.length,c.length);
+	}
+    }
+    return "";
+} 
+var thisisdacookie2 = "  Time spent doing push-ups: " + getCookie("timespentpushups") + " minutes";
+var thisisdacookie3 = "  Push-ups done " + getCookie("pushupsdone") + " times";
+var thisisdacookie = "  Time spent outside: " + getCookie("outsidetime") + " minutes";
+document.getElementById('timespentpushups').innerHTML=thisisdacookie2;
+document.getElementById('pushupsdone').innerHTML=thisisdacookie3;
+document.getElementById('timeoutside').innerHTML=thisisdacookie;
