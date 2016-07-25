facf844fc4d34bc17885c66094dd260eba579806
githubcyclist <mouse869@gmail.com>
2016-07-25 09:48:05 -0400

    Fixed bug

--- writecookie.js
+++ writecookie.js
@@ -1,4 +1,18 @@
 // original name mor3efunction.js
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+	var c = ca[i];
+	while (c.charAt(0)==' ') {
+		c = c.substring(1);
+	}
+	if (c.indexOf(name) == 0) {
+		return c.substring(name.length,c.length);
+	}
+    }
+    return "";
+}
 function write() {
 	document.cookie = "outsidetime=" + document.getElementById('minspentoutside').value + ";" + "expires=Thu, 1 Jan 3000 12:00:00 UTC;";
 	document.cookie = "timespentpushups=" + document.getElementById('minspentpushups').value + ";" + "expires=Thu, 1 Jan 3000 12:00:00 UTC;";
