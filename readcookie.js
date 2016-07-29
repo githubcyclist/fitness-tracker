c6485412acd9224ef89cc21533bdaefafb8b8833
githubcyclist <mouse869@gmail.com>
2016-07-29 09:26:47 -0400

    Update readcookie.js

--- readcookie.js
+++ readcookie.js
@@ -16,18 +16,21 @@ function add(a,b){
     return a-(-b);
 }
 function read() {
-var mydomstorage=window.localStorage || (window.globalStorage? globalStorage[location.hostname] : null)
+
 var thisisdacookie = "  Time spent outside: " + getCookie("outsidetime") + " minutes";
 var thisisdacookie2 = "  Time spent doing push-ups: " + getCookie("timespentpushups") + " minutes";
 var thisisdacookie3 = "  Push-ups done " + getCookie("pushupsdone") + " times";
 var thisisdacookie4 = "  Time spent doing pull-ups: " + getCookie("timespentpullups") + " minutes";
 var thisisdacookie5 = "  Pull-ups done " + getCookie("pullupsdone") + " times";
+var thisisdacookie6 = "  Exercised " + getCookie("timesexercised") + " times";
+/*Old code to read localstorage
+var mydomstorage=window.localStorage || (window.globalStorage? globalStorage[location.hostname] : null)
 if(mydomstorage) {
 	var thisisdacookie6 = "  Exercised " + parseInt(localstorage.getItem('timesexercised')) + " times";
 	document.getElementById('timesexercised').innerHTML=thisisdacookie6;
 } else {
 	document.write("Sorry, this browser does not support DOM storage.");
-}
+}*/
 document.getElementById('timespentpushups').innerHTML=thisisdacookie2;
 document.getElementById('pushupsdone').innerHTML=thisisdacookie3;
 document.getElementById('timeoutside').innerHTML=thisisdacookie;
