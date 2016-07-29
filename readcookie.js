c591836995b8c6a84bdd0113f846c3b35bc46792
githubcyclist <mouse869@gmail.com>
2016-07-28 21:13:49 -0400

    Update readcookie.js

--- readcookie.js
+++ readcookie.js
@@ -22,11 +22,20 @@ var thisisdacookie2 = "  Time spent doing push-ups: " + getCookie("timespentpush
 var thisisdacookie3 = "  Push-ups done " + getCookie("pushupsdone") + " times";
 var thisisdacookie4 = "  Time spent doing pull-ups: " + getCookie("timespentpullups") + " minutes";
 var thisisdacookie5 = "  Pull-ups done " + getCookie("pullupsdone") + " times";
-var thisisdacookie6 = "  Exercised " + parseInt(localstorage.getItem('timesexercised')) + " times";
+if(mydomstorage) {
+	var thisisdacookie6 = "  Exercised " + parseInt(localstorage.getItem('timesexercised')) + " times";
+} else {
+	
+}
 document.getElementById('timespentpushups').innerHTML=thisisdacookie2;
 document.getElementById('pushupsdone').innerHTML=thisisdacookie3;
 document.getElementById('timeoutside').innerHTML=thisisdacookie;
 document.getElementById('timespentpullups').innerHTML=thisisdacookie4;
 document.getElementById('pullupsdone').innerHTML=thisisdacookie5;
-//document.getElementById('timesexercised').innerHTML=thisisdacookie6;
+if (mydomstorage) {
+	document.getElementById('timesexercised').innerHTML=thisisdacookie6;
+} else {
+	document.write("Sorry, this browser does not support DOM storage.");
+}
+}
 }
