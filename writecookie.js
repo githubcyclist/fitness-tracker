a078fe7bc35a451480ba34c79bf2bcf9805ec62b
githubcyclist <mouse869@gmail.com>
2016-07-30 14:29:51 -0400

    Debugging JavaScript code

--- writecookie.js
+++ writecookie.js
@@ -51,7 +51,7 @@ function write() {
 }
 function reset() {
 	var mydomstorage=window.localStorage || (window.globalStorage? globalStorage[location.hostname] : null)
-	if(mydomstorage){
+	if(window.localStorage){
 		localstorage.removeItem('timesexercised');
 	} else {
 		document.write("Sorry, this browser does not support DOM storage.");
